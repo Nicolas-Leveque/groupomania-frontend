5f4baec1e3deb8ae51fcc662d2f4939122930857
Nicolas Lévêque <nicoleveque@outlook.com>
2021-12-14 17:21:12 +0100

    Ajout context api et info head html

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import { useState } from "react"
+import AppContext from "../AppContext"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [reload, setReload] = useState(false)
+  const [token, setToken ] = useState('')
+  const [userId, setUserId ] = useState('')
+  const [isAdmin, setIsAdmin ] = useState()
+  return (
+      <AppContext.Provider
+        value={{ token, setToken, userId, setUserId, reload, setReload, isAdmin, setIsAdmin }}
+      >
+        <Component {...pageProps} />
+      </AppContext.Provider>
+    )
 }
 
 export default MyApp
